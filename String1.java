23fd20467071225b091b9cba8ec2bba99b115f04
Rohan <111503422+GamesGosling427@users.noreply.github.com>
2022-08-20 13:00:23 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,15 @@
+package string;
+
+public class String1 {
+
+	public static void main(String[] args) {
+
+	String firstName="tony";
+	String lastName ="stark";
+	//String fullName=firstName+lastName;
+	String fulName=firstName+  "@" + lastName;
+	System.out.println(fulName.length());
+	
+	}
+
+}
